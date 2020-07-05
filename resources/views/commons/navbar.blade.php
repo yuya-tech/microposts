e7cf67d61fde5bd83df6ae816c3afe33b90b48cf
yuya.ishimaru <genmaimai140@gmail.com>
2020-07-05 14:20:41 +0000

    navber

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,15 +9,20 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                @if (Auth::check())
+               @if (Auth::check())
                     <li class="nav-item">{!! link_to_route('users.index', 'Users', [], ['class' => 'nav-link']) !!}</li>
                     <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                       <ul class="dropdown-menu dropdown-menu-right">
+                                <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
+                                <li class="dropdown-divider"></li>
+                                <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                    </li>
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
                 @endif
-            </ul>
-            <ul class="dropdown-menu dropdown-menu-right">
-                <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
-                <li class="dropdown-divider"></li>
-                <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
             </ul>
         </div>
     </nav>
