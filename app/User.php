d22fbeec05c6fd72eec186df476fea11d3d5cdec
yuya_10969 <genmaimai140@gmail.com>
2020-07-10 23:48:46 +0900

    user.phpとfavoritesタブ追加とfavorites.bladeのコンポーメントを変更

--- app/User.php
+++ app/User.php
@@ -26,12 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
+
     public function microposts()
     {
         return $this->hasMany(Micropost::class);
     }
-    
+
     public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
@@ -48,7 +48,7 @@ class User extends Authenticatable
         $exist = $this->is_following($userId);
         // 相手が自分自身ではないかの確認
         $its_me = $this->id == $userId;
-    
+
         if ($exist || $its_me) {
             // 既にフォローしていれば何もしない
             return false;
@@ -58,14 +58,14 @@ class User extends Authenticatable
             return true;
         }
     }
-    
+
     public function unfollow($userId)
     {
         // 既にフォローしているかの確認
         $exist = $this->is_following($userId);
         // 相手が自分自身かどうかの確認
         $its_me = $this->id == $userId;
-    
+
         if ($exist && !$its_me) {
             // 既にフォローしていればフォローを外す
             $this->followings()->detach($userId);
@@ -75,22 +75,21 @@ class User extends Authenticatable
             return false;
         }
     }
-    
+
     public function is_following($userId)
     {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
-    
+
     public function feed_microposts()
     {
         $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
         $follow_user_ids[] = $this->id;
         return Micropost::whereIn('user_id', $follow_user_ids);
     }
-    
-     public function favorites()
-     {
-         return $this->belongsToMany(User::class);
-     }
-     
+
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'user_id', 'micropost_id')->withTimestamps();
+    }
 }
